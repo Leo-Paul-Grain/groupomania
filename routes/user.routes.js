7ce4877768ebed4449e5de3e4b78927fd9dd215f
Léo-Paul Grain <leopaul.grain@gmail.com>
2022-09-21 18:51:31 +0200

    install and partial config of multer

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,16 +2,22 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller.js');
 const userController = require('../controllers/user.controller');
+const uploadController = require('../controllers/upload.controller');
+const multer = require('multer');
+const upload = multer();
 
 //Routes d'authentification
 router.post('/register', authController.signUp);
 router.post('/login', authController.signIn);
 router.get('/logout', authController.logout);
 
-//Routes liés aux Users
+//Read, update, delete Users
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getOneUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
+
+//upload image de profil
+router.post('/upload', upload.single('file'), uploadController.uploadProfil);
 
 module.exports = router;
--- utils/error.utils.js
+++ utils/error.utils.js
@@ -46,3 +46,15 @@ module.exports.signInErrors = (err) => {
 
     return errors
 };
+
+module.exports.uploadErrors = (err) => {
+    let errors = { format: '', maxSize: '' };
+
+    if (err.message.includes('invalid file'))
+        errors.format = 'Format incompatible';
+
+    if (err.message.includes('max size exceeded'))
+        errors.maxSize = 'Le fichier dépasse 2mo';
+    
+    return errors
+};
